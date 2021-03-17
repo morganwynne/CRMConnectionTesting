1618e224657e28d8dc2f3f23563e70993d83158f
Morgan Wynne <armorproof@hotmail.com>
2021-03-17 09:22:12 -0700

    Organizing Resource Constants
    Reorganized the Dynamics CRM resources as variables so I can reference them easier.

--- CRMConnectionTesting/Program.cs
+++ CRMConnectionTesting/Program.cs
@@ -17,22 +17,26 @@ namespace CRMConnectionTesting
     // Build Windows client applications using the XRM tools https://docs.microsoft.com/en-us/powerapps/developer/data-platform/xrm-tooling/build-windows-client-applications-xrm-tools
     // Sample https://github.com/microsoft/PowerApps-Samples/tree/master/cds/Xrm%20Tooling/Quick%20start%20for%20XRM%20Tooling%20API
 
-    // Instance Web API: https://mediabardsandbox.api.crm3.dynamics.com/api/data/v9.2/
-    // Instance Reference ID: 312d3ad4-e79e-49f9-8719-3b98188d3460
-    // Instance Reference Unique Name: 312d3ad4e79e49f987193b98188d3460
-    // Discovery Web API: https://disco.crm3.dynamics.com/api/discovery/v9.2/
-    // Organization Service: https://mediabardsandbox.api.crm3.dynamics.com/XRMServices/2011/Organization.svc
-    // Discovery Service: https://disco.crm3.dynamics.com/XRMServices/2011/Discovery.svc
-
     // CRMServiceProxy Connection Method https://arunpotti.com/2014/12/09/connect-to-crm-online-or-on-premise-using-c/
 
     public class Program
     {
-        static IOrganizationService _service;
+        private static IOrganizationService _service;
 
-        static void Main(string[] args)
+        private const string DynamicsCRM_DiscoveryWebAPI = "https://disco.crm3.dynamics.com/api/discovery/v9.2/";
+        private const string DynamicsCRM_OrganizationService = "https://mediabardsandbox.api.crm3.dynamics.com/XRMServices/2011/Organization.svc";
+        private const string DynamicsCRM_DiscoveryService = "https://disco.crm3.dynamics.com/XRMServices/2011/Discovery.svc";
+
+        private const string DynamicsCRM_Mediabard_InstanceWebAPI = "https://mediabardsandbox.api.crm3.dynamics.com/api/data/v9.2/";
+        private const string DynamicsCRM_Mediabard_InstanceReferenceID = "312d3ad4-e79e-49f9-8719-3b98188d3460";
+        private const string DynamicsCRM_Mediabard_InstanceReferenceUniqueName = "312d3ad4e79e49f987193b98188d3460";
+        
+        private const string DynamicsCRM_Mediabard_TestUserUsername = "testuser@mediabard.onmicrosoft.com";
+        private const string DynamicsCRM_Mediabard_TestUserPassword = "Thisis1userfortesting";
+
+        public static void Main(string[] args)
         {
-            ConnectToMSCRM( "testuser@mediabard.onmicrosoft.com", "Thisis1userfortesting", "https://mediabardsandbox.api.crm3.dynamics.com/XRMServices/2011/Organization.svc" );
+            ConnectToMSCRM( DynamicsCRM_Mediabard_TestUserUsername, DynamicsCRM_Mediabard_TestUserPassword, DynamicsCRM_OrganizationService );
 
             Guid userid = ( (WhoAmIResponse)_service.Execute( new WhoAmIRequest() ) ).UserId;
             if( userid != Guid.Empty )
@@ -64,7 +68,7 @@ namespace CRMConnectionTesting
         /// <summary>
         /// Connect the Organization Service to the corresponding organization using the given username and password.
         /// </summary>
-        public static void ConnectToMSCRM( string UserName, string Password, string SoapOrgServiceUri )
+        private static void ConnectToMSCRM( string UserName, string Password, string SoapOrgServiceUri )
         {
             try
             {
